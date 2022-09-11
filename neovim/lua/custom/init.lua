3fffd164ed065687e6fc331c7a17558cdd27d6b2
Aleksey Boldyrev <aleksey.boldyrev@gmail.com>
2022-09-11 21:16:49 +0300

    Neovim - add relative numbers

--- neovim/lua/custom/init.lua
+++ neovim/lua/custom/init.lua
@@ -3,4 +3,5 @@ require 'custom.autocmds'
 vim.opt.wrap = false
 vim.opt.scrolloff = 8
 vim.opt.sidescrolloff = 8
+vim.opt.relativenumber = true
 vim.opt.langmap = "ФИСВУАПРШОЛДЬТЩЗЙКЫЕГМЦЧНЯ;ABCDEFGHIJKLMNOPQRSTUVWXYZ,фисвуапршолдьтщзйкыегмцчня;abcdefghijklmnopqrstuvwxyz"
