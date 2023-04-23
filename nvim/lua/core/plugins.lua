dd47b0aeac452dcb5b992ccfb54b62cbccfbc57d
Aleksey Boldyrev <aboldyrev@rdg.ai>
2023-04-23 14:54:27 +0300

    Add harpoon plugin

--- nvim/lua/core/plugins.lua
+++ nvim/lua/core/plugins.lua
@@ -20,6 +20,7 @@ return require('packer').startup(function(use)
   use 'nvim-treesitter/nvim-treesitter'
   use 'bluz71/vim-nightfly-colors'
   use 'lewis6991/gitsigns.nvim'
+  use 'theprimeagen/harpoon'
 
   use {
     "williamboman/mason.nvim",
