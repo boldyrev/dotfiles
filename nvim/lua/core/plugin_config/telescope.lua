0ca18abc825d397ba38b7d382913c43353be8b05
Aleksey Boldyrev <aleksey.boldyrev@gmail.com>
2023-10-22 00:21:43 +0300

    Nvim update telescope layout and key maps

--- nvim/lua/core/plugin_config/telescope.lua
+++ nvim/lua/core/plugin_config/telescope.lua
@@ -1,8 +1,20 @@
-require('telescope').setup()
+require('telescope').setup {
+	defaults = {
+	  layout_config = {
+      horizontal = {
+        preview_cutoff = 140,
+        width = 0.95,
+      },
+    },
+    file_ignore_patterns = { ".git/.*", ".idea/.*" , "__pycache__/.*" },
+	}
+}
 local builtin = require('telescope.builtin')
 
-vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
+vim.keymap.set('n', '<leader>ff', '<cmd> Telescope find_files previewer=false hidden=true no_ignore=true<CR>', {})
+-- show usage
+vim.keymap.set('n', '<leader>fu', '<cmd> Telescope lsp_references show_line=false<CR>', {})
 vim.keymap.set('n', '<leader>fo', builtin.oldfiles, {})
 vim.keymap.set('n', '<leader>fw', builtin.live_grep, {})
-vim.keymap.set('n', '<leader>fb', '<cmd> Telescope buffers <CR>', {})
-vim.keymap.set('n', '<leader>fa', '<cmd> Telescope find_files follow=true no_ignore=true hidden=true <CR>', {})
+vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
+-- vim.keymap.set('n', '<leader>fa', '<cmd> Telescope find_files follow=true no_ignore=true hidden=true <CR>', {})
