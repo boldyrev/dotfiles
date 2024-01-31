54dbefe0d246ebe8c0c4a580cb849187fd428d98
Aleksey Boldyrev <aleksey.boldyrev@gmail.com>
2024-01-31 18:21:22 +0300

    nvim, add todo-comments

--- nvim/lua/core/plugins.lua
+++ nvim/lua/core/plugins.lua
@@ -137,4 +137,17 @@ require('lazy').setup({
       require("trouble").setup {}
     end
   },
+
+  {
+    "folke/todo-comments.nvim",
+    dependencies = "nvim-lua/plenary.nvim",
+    lazy = false,
+    config = function()
+      require("todo-comments").setup {
+        -- your configuration comes here
+        -- or leave it empty to use the default settings
+        -- refer to the configuration section below
+      }
+    end
+  },
 })
