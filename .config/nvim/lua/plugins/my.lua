8e4071e9d5e99f314c1b74afd6cd8314a91cffd9
Alex <alex@MacBook-Air-Alex.local>
2025-12-02 09:31:13 +0300

    nvim: disable autocomplite

--- .config/nvim/lua/plugins/my.lua
+++ .config/nvim/lua/plugins/my.lua
@@ -53,4 +53,48 @@ return {
       },
     },
   },
+  {
+    "saghen/blink.cmp",
+    -- Make blink.cmp toogleable
+    opts = function(_, opts)
+      vim.b.completion = false
+
+      Snacks.toggle({
+        name = "Completion",
+        get = function()
+          return vim.b.completion
+        end,
+        set = function(state)
+          vim.b.completion = state
+        end,
+      }):map("<leader>uk")
+
+      opts.enabled = function()
+        return vim.b.completion ~= false
+      end
+      return opts
+    end,
+  },
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      servers = {
+        gopls = {
+          settings = {
+            gopls = {
+              hints = {
+                assignVariableTypes = false,
+                compositeLiteralFields = true,
+                compositeLiteralTypes = false,
+                constantValues = true,
+                functionTypeParameters = false,
+                parameterNames = false,
+                rangeVariableTypes = false,
+              },
+            },
+          },
+        },
+      },
+    },
+  },
 }
